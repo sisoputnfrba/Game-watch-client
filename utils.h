a95642c17d192e2b41967e2a09a0621259aad3bc
mgarciaisaia <mgarciaisaia+github@gmail.com>
2020-03-28 19:53:05 -0300

    Expongo liberar_conexion en el utils.h
    La necesitamos desde el codigo principal, asi que tiene que
    exportarse en el .h

--- utils.h
+++ utils.h
@@ -37,5 +37,6 @@ int crear_conexion(char* ip, char* puerto);
 void enviar_mensaje(char* mensaje, int socket_cliente);
 char* recibir_mensaje(int socket_cliente);
 void eliminar_paquete(t_paquete* paquete);
+void liberar_conexion(int socket_cliente);
 
 #endif /* UTILS_H_ */
